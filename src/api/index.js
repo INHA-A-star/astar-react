c3d109bb55171b837066a1bbffcc7d3c004601dc
DESKTOP-EBVP9FP\kdh <mdiu2203@naver.com>
2021-11-25 14:17:21 +0900

    proxy middleware set

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function createInstance() {
   return axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? '/' : process.env.REACT_APP_API_URL
+    baseURL: process.env.NODE_ENV === 'development' ? '/api/' : `${process.env.REACT_APP_API_URL}/api/`
   });
 }
 
