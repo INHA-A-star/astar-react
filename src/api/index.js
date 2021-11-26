5ced0056f0f17e37619b3d79bcccdbd067cd8721
seonno, lee <lsno1106@gmail.com>
2021-11-26 19:08:05 +0900

    nginx proxy 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 function createInstance() {
   return axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/api`
+    baseURL: `${process.env.REACT_APP_API_URL}`,
   });
 }
 
