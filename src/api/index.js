4bed13c2354677955eaf5d4f2a7829e25d7d274c
DESKTOP-EBVP9FP\kdh <mdiu2203@naver.com>
2021-11-27 03:03:27 +0900

    base url 변경

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 function createInstance() {
   return axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/api`
-    // baseURL: '/api',
+    baseURL: `${process.env.REACT_APP_API_URL}`
   });
 }
 
